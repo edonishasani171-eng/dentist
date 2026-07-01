723c37f6fc0a2348ce82830fbf6add70b3f9d588
Edonis Hasani <edonishasani171@gmail.com>
2026-07-01 16:52:37 +0200

    Fixing the Regjistro Puntorin layout bug

--- api/admin_dashboard.php
+++ api/admin_dashboard.php
@@ -1082,13 +1082,13 @@ try {
                     Oraret
                 </a>
             </li>
+            <li class="menu-item <?= $current_page === 'register_worker' ? 'active' : '' ?>">
+                <a href="register_worker.php">
+                    <svg viewBox="0 0 24 24"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>
+                    Regjistro Punëtor
+                </a>
+            </li>
         </ul>
-        <li class="menu-item <?= $current_page === 'register_worker' ? 'active' : '' ?>">
-            <a href="register_worker.php">
-                <svg viewBox="0 0 24 24"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>
-                Regjistro Punëtor
-            </a>
-        </li>
 
         <div class="menu-item logout-btn">
             <a href="logout.php" style="color: #c0392b;">
